d837ed4a701fe4c22e6f5d922c3706379d292936
Tomas Sarmiento <t.sarmiento@loro.com.co>
2022-01-06 18:03:14 -0500

    contracts and data sources

---
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,28 @@
+import 'package:clean_architecture_tdd_course/features/core/error/failures.dart';
+import 'package:clean_architecture_tdd_course/features/core/platform/network_info.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class NumberTriviaRepositoryImplementation implements NumberTriviaRepository {
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImplementation(
+      {required this.remoteDataSource,
+      required this.localDataSource,
+      required this.networkInfo});
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
+    throw UnimplementedError();
+  }
+}
